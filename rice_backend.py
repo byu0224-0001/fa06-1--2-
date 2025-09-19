c9aec323e8153f5460fb38f559681f0a1d1dd7c1
byu0224-0001 <byu0224-0001@example.com>
2025-09-19 10:01:11 +0900

    IndentationError 수정 - MovingAverage 클래스 메서드 들여쓰기 오류 해결

--- rice_backend.py
+++ rice_backend.py
@@ -55,18 +55,18 @@ def _get_device():
 # -------------------------
 if nn is not None:
     class MovingAverage(nn.Module):
-    def __init__(self, kernel_size, stride=1):
-        super(MovingAverage, self).__init__()
-        self.kernel_size = kernel_size
-        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+        def __init__(self, kernel_size, stride=1):
+            super(MovingAverage, self).__init__()
+            self.kernel_size = kernel_size
+            self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
 
-    def forward(self, x):
-        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        x = torch.cat([front, x, end], dim=1)
-        x = self.avg(x.permute(0, 2, 1))
-        x = x.permute(0, 2, 1)
-        return x
+        def forward(self, x):
+            front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+            end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+            x = torch.cat([front, x, end], dim=1)
+            x = self.avg(x.permute(0, 2, 1))
+            x = x.permute(0, 2, 1)
+            return x
 
     class DLinear(nn.Module):
         def __init__(self, input_length, output_length, input_dim, kernel_size=25):
